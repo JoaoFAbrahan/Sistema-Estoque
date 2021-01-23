109881765a115b115f3dcd63c970cfec5131b1c6
Joao Felipe Abrahan <joaofelipeabrahan@hotmail.com>
2021-01-23 12:07:25 -0400

    Pré definição do projeto e adição de informações

---
+++ Sistema-Estoque/Headers.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<Windows.h>
+#include<iostream>
+#include<locale>
+
+//Structs do Programa
+
+
+
+//Classes do Programa
+
+
+
+//Variáveis Globais
+
+
+
+//Protótipos de Funções
---
+++ Sistema-Estoque/Main.cpp
@@ -0,0 +1,18 @@
+/*
+	######################################################################
+	##########                SISTEMA DE ESTOQUE                ##########
+	######################################################################
+
+	  Sistema de estoque em C++ desenvolvido com o objetivo de estudo de 
+	Orientação a Objetos e utilização de Interface Gráfica da biblioteca
+	<Windows.h>.
+
+	DESENVOLVEDORES:		João Felipe Abrahan,
+							Jorge Calligopoulo.
+
+	DATA DESENVOLVIMENTO:	23/01/2021 até XX/XX/2021.
+	VERSÃO DA LINGUAGEM:	C++17.
+	TIPO DE LICENÇA:		MIT.
+*/
+
+
